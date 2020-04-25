6467f34ba5255e2a0eb19a2487ebf961a8f6bfe4
ibrahim hamzat <hamat.ibrahim3@gmail.com>
2020-04-25 10:47:41 +0100

    app pushed

--- src/App.js
+++ src/App.js
@@ -4,22 +4,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <div className="navbar-fixed">
+    <nav>
+      <div className="nav-wrapper">
+        <a href="#!" className="brand-logo">Logo</a>
+        <ul className="right hide-on-med-and-down">
+          <li><a href="sass.html">Sass</a></li>
+          <li><a href="badges.html">Components</a></li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+        
   );
 }
 
